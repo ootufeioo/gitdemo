dfbb1aa1e741fbe8c2f4f1678de78d3a913883d8
admin <admin@11.com>
2020-03-29 21:44:21 +0800

    first

--- src/com/liu/git/gitDemo.java
+++ src/com/liu/git/gitDemo.java
@@ -4,5 +4,6 @@ public class gitDemo {
     public static void main(String[] args) {
         System.out.println("git first");
         System.out.println("修改后");
+        System.out.println("clone 后 修改");
     }
 }
